91a9c673f4efd13b217f1c1c210f7e046ac207ea
ekaleto <rolete1cfara2himete20@gmail.com>
2012-10-31 20:14:30 +0900

    RoutingTable.java Host.java　追加

---
+++ src/gnutella/Host.java
@@ -0,0 +1,11 @@
+package gnutella;
+
+import gnutella.message.GUID;
+
+public class Host {
+	private Connection connection;
+	private GUID guid;
+	private int shareFileCount;
+	private List<SharedFile> shareFileList;
+
+}
---
+++ src/gnutella/RoutingProtocol.java
@@ -0,0 +1,5 @@
+package gnutella;
+
+public class RoutingProtocol {
+
+}
