04ef6323ff7a56fcce8f929e51ea11b8747e8748
ekaleto <rolete1cfara2himete20@gmail.com>
2012-11-03 11:58:35 +0900

    GUID完成

--- src/gnutella/message/GUID.java
+++ src/gnutella/message/GUID.java
@@ -1,5 +1,32 @@
 package gnutella.message;
 
-public class GUID {
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
 
+public class GUID {
+	private byte[] guid;
+
+
+	public byte[] GUID(InetAddress ipAddress){
+		byte[] guid = new byte[16];
+		guid = ipAddress.getAddress();
+		return guid;
+	}
+
+	@Override
+	public int hashCode(){
+		ByteBuffer wb = ByteBuffer.wrap(this.guid);
+		int intGuid = wb.getInt();
+		return intGuid;
+	}
+
+	@Override
+	public boolean equals(Object o){
+		if(o == null) return false;
+		if(o == this) return true;
+		if(o.getClass() != getClass()) return false;
+		GUID ex = (GUID)o;
+		return this.hashCode() == ex.hashCode() && this.guid.equals(o);
+	}
 }
+
