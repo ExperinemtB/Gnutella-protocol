72faf89750938a42369d59320f3144ca5dc1554e
ekaleto <rolete1cfara2himete20@gmail.com>
2012-11-07 01:17:18 +0900

    pongMessage.java　仮追加2

--- src/gnutella/message/PongMessage.java
+++ src/gnutella/message/PongMessage.java
@@ -1,6 +1,33 @@
 package gnutella.message;
 
+import java.nio.ByteBuffer;
+
 public class PongMessage extends Message {
+	int port, ipAddress, numberOfFilesShared, numberOfKilobytesShared;
 	public PongMessage(Header header, byte[] payload) {
+		byte[] data = new byte[4];
+
+		data[0] = payload[0];
+		data[1] = payload[1];
+		ByteBuffer wb0 = ByteBuffer.wrap(data);
+		this.port = wb0.getInt();
+
+		for(int i = 0; i < 4; i++){
+			data[i] = payload[2+i];
+		}
+		ByteBuffer wb1 = ByteBuffer.wrap(data);
+		this.ipAddress = wb1.getInt();
+
+		for(int i = 0; i < 4; i++){
+			data[i] = payload[6+i];
+		}
+		ByteBuffer wb2 = ByteBuffer.wrap(data);
+		this.numberOfFilesShared = wb2.getInt();
+
+		for(int i = 0; i < 4; i++){
+			data[i] = payload[10+i];
+		}
+		ByteBuffer wb3 = ByteBuffer.wrap(data);
+		this.numberOfKilobytesShared = wb3.getInt();
 	}
 }
