65c31060b6dfb664d6a920cea555f2ba4c745a3d
ekaleto <rolete1cfara2himete20@gmail.com>
2012-11-07 11:27:04 +0900

    test

--- src/gnutella/message/Header.java
+++ src/gnutella/message/Header.java
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-package gnutella.message;
-
-public class Header {
-	public enum PayloadDescriptorType {
-		PING,PONG,PUSH,QUERY,QUERYHIT;
-	}
-	public static final int HEADER_LENGTH = 23;
-
-	public static final int PING = 0x00;
-	public static final int PONG = 0x01;
-	public static final int PUSH = 0x40;
-	public static final int QUERY = 0x80;
-	public static final int QUERYHIT = 0x81;
-
-	private GUID guid;
-	private PayloadDescriptorType payloadDescriptor;
-	private int ttl;
-	private int hops;
-	private int payloadLength;
-
-	public Header(){
-	}
-
-	public GUID getGuid() {
-		return guid;
-	}
-	public void setGuid(GUID guid) {
-		this.guid = guid;
-	}
-	public PayloadDescriptorType getPayloadDescriptor() {
-		return payloadDescriptor;
-	}
-	public void setPayloadDescriptor(PayloadDescriptorType payloadDescriptor) {
-		this.payloadDescriptor = payloadDescriptor;
-	}
-	public int getTtl() {
-		return ttl;
-	}
-	public void setTtl(int ttl) {
-		this.ttl = ttl;
-	}
-	public int getHops() {
-		return hops;
-	}
-	public void setHops(int hops) {
-		this.hops = hops;
-	}
-	public int getPayloadLength() {
-		return payloadLength;
-	}
-	public void setPayloadLength(int payloadLength) {
-		this.payloadLength = payloadLength;
-	}
-
-	public byte[] getBytes(){
-		return new byte[0];
-	}
-
-	public static Header parse(byte[] data){
-		if(data.length<HEADER_LENGTH){
-			return null;
-		}
-
-		return new Header();
-	}
-}
-=======
 package gnutella.message;
 
 import java.nio.ByteBuffer;
@@ -157,4 +89,3 @@ public class Header {
 		return new Header(new GUID(guid), payload, ttl, hops, payloadLength);
 	}
 }
->>>>>>> origin/tempbranch
