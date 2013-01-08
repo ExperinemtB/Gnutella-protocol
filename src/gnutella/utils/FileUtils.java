7dabdd18346492d287a11481eefe6ce679cdaadd
pxsta <pxsta.fk@gmail.com>
2013-01-09 00:32:55 +0900

    バッファサイズを対象のファイルサイズに応じた大きさにするように変更
    パフォーマンス向上のため

--- src/gnutella/utils/FileUtils.java
+++ src/gnutella/utils/FileUtils.java
@@ -10,7 +10,7 @@ public class FileUtils {
 	public static byte[] getFileMD5Digest(File file) throws NoSuchAlgorithmException, IOException {
 		MessageDigest md = MessageDigest.getInstance("MD5");
 		FileInputStream in = new FileInputStream(file);
-		byte[] date = new byte[256];
+		byte[] date = new byte[(int)Math.min(536870912,Math.max(256,file.length()/10))];
 		int len;
 		while ((len = in.read(date)) >= 0) {
 			md.update(date, 0, len);
